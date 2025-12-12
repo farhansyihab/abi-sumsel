a88cecf43169950554c24f94b6ed61e12343b85b
farhansyihab <agiptek@gmail.com>
2025-12-13 05:25:50 +0700

    perbaikan akses

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -6,6 +6,15 @@ DEBUG = False
 # outdated JavaScript / CSS assets being served from cache
 # (e.g. after a Wagtail upgrade).
 # See https://docs.djangoproject.com/en/6.0/ref/contrib/staticfiles/#manifeststaticfilesstorage
+DEBUG = False
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-lbw3h)u8a)b+tuz3td%(b4+9d4826i$0jmnf1@)zhyqpb0pp)-"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 STORAGES["staticfiles"]["BACKEND"] = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 try:
